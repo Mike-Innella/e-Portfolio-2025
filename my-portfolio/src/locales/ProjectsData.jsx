f8fadcb8e3d0a4488cca04d4335ee22753f4c5c7
Michael Innella <62763951+Mike-Innella@users.noreply.github.com>
2025-06-04 16:20:55 -0400

    project data edit

--- my-portfolio/src/locales/ProjectsData.jsx
+++ my-portfolio/src/locales/ProjectsData.jsx
@@ -44,7 +44,7 @@ const ProjectsData = [
     name: "The Shaman Asher",
     icon: Icons["sun"],
     description:
-      "Brought in post-launch to elevate the UI/UX of a mystical services site. Enhanced the interface with skeleton loading states, a custom animated message loader, and interactive menu underlines. My ongoing contributions include accessibility improvements, refined responsive design, and additional UI/UX polish.",
+      "Hired on a freelance basis to elevate the UI/UX of a mystical services site. Enhanced the interface with skeleton loading states, a custom animated message loader, and interactive menu underlines. My ongoing contributions include accessibility improvements, refined responsive design, and additional UI/UX polish.",
     techStack: ["typescript", "tailwind", "emailjs", "html", "nodejs"],
     links: [
       {
